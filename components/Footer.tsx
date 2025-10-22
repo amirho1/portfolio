39b2335abecb83b442b819790cee85e95044b5c0
Amirhossein Salighedar <amirhossein.s@arianalabs.io>
2025-10-22 14:47:06 +0330

    fix(footer): separate the current year to fix hydration error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
+import CurrentYear from "./CurrentYear";
+
 export default function Footer() {
-  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-gray-50 py-8">
       <div className="container mx-auto px-4 text-center">
         <p className="text-gray-600">
-          © {currentYear} Amir Hossein Salighedar. All rights reserved.
+          © <CurrentYear /> Amir Hossein Salighedar. All rights reserved.
         </p>
       </div>
     </footer>
